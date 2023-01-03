7e0e810f6428b3c2d53254852d7572031875f92d
Mirko Wust <mirko@Air-von-Mirko.fritz.box>
2023-01-03 13:02:41 +0100

    apis

--- apis/send2opensensemap.py
+++ apis/send2opensensemap.py
@@ -39,16 +39,14 @@ def getOSMTimestamp():
 
 
 #def postOSMvalues(sensorID, val, ts):
-def postOSMvalues(data):
+def postOSMvalues(payload):
    print('-------postOSMvalues-------')
 
    # url = osm_url + f'{senseBoxID}/{sensorID}'
    url = osm_url + f'{senseBoxID}/data'
    # payload = json.dumps({"value": f"{val}", "createdAt": f"{ts}"})
    # payload = {"value": f"{val}", "createdAt": f"{ts}"}
-   payload = data
    headers = {'Authorization': f'{OpenSenseMap_TOKEN}', 'Content-Type': 'application/json'}
-
    print(url, payload)
    
    
@@ -66,5 +64,5 @@ def postOSMvalues(data):
    if err_code != 1:
       # data = f'{sensorID}, {payload}'
       f = open(cachefile, mode='a', encoding='utf-8')
-      f.write(f'{data}\n')
+      f.write(f'{json.dumps(payload)}\n')
       f.close()
