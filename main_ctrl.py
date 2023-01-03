9a7a6c600a7298a5d6e7cc6a406ab1edcb1f017e
Mirko Wust <mirko@Air-von-Mirko.fritz.box>
2023-01-03 19:11:27 +0100

    apis

--- main_ctrl.py
+++ main_ctrl.py
@@ -1,3 +1,4 @@
+import time
 from get_bme280 import *
 from get_dht22 import *
 from get_sound import *
@@ -6,9 +7,11 @@ from get_tsl45315 import *
 from get_veml6070 import *
 
 
-read_bme280()
-read_dht22()
-read_sound()
-read_sps30()
-read_tsl45315()
-read_veml6070()
+while True:
+    read_bme280()
+    read_dht22()
+    read_sound()
+    read_sps30()
+    read_tsl45315()
+    read_veml6070()
+    time.sleep(60)
