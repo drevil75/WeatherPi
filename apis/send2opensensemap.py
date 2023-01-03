1a93a9e3fe1e21c25509ecac0090c4a9b9f37d61
Mirko Wust <mirko@Air-von-Mirko.fritz.box>
2023-01-03 17:55:45 +0100

    apis

--- apis/send2opensensemap.py
+++ apis/send2opensensemap.py
@@ -47,19 +47,19 @@ def postOSMvalues(payload):
    print(url, payload)
    
 
-   try:
-      # r = requests.post(url, headers=headers, data=payload, timeout=10)
-      r = requests.post(url, headers=headers, json=payload, timeout=10)
-      print(f'rc={r.status_code}')
-      if r.status_code in [200, 201, 202, 203, 204]:
-         err_code = 1
-      else:   
-         err_code = r.text
-   except:
-      err_code = 'timeout'
+   # try:
+   #    # r = requests.post(url, headers=headers, data=payload, timeout=10)
+   r = requests.post(url, headers=headers, json=payload, timeout=10)
+   print(r)
+   #    if r.status_code in [200, 201, 202, 203, 204]:
+   #       err_code = 1
+   #    else:   
+   #       err_code = r.text
+   # except:
+   #    err_code = 'timeout'
 
-   if err_code != 1:
-      # data = f'{sensorID}, {payload}'
-      f = open(cachefile, mode='a', encoding='utf-8')
-      f.write(f'{json.dumps(payload)}\n')
-      f.close()
+   # if err_code != 1:
+   #    # data = f'{sensorID}, {payload}'
+   #    f = open(cachefile, mode='a', encoding='utf-8')
+   #    f.write(f'{json.dumps(payload)}\n')
+   #    f.close()
