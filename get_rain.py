f46008a42481bada50af461b67135759b8ea41b8
Mirko Wust <mirko@Air-von-Mirko.fritz.box>
2023-01-04 15:56:49 +0100

    wind and rain

--- get_rain.py
+++ get_rain.py
@@ -8,6 +8,7 @@ from apis.send2openhab import *
 # GPIO-Ports
 Counter_Rain = 0
 pin_rain = 19
+rainvolume = 0
 volPerKlick = 0.2995357196345664 #0,29... mm Regen pro qm =
 
 # Zaehlvariable
@@ -25,33 +26,35 @@ GPIO.setup(pin_rain, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 def isr_rain(channel):  
     global Counter_Rain
     Counter_Rain += 1
-#    print("Counter_Rain: %d" % Counter_Rain)
+    print("Counter_Rain: %d" % Counter_Rain)
 
 # Interrupts aktivieren
 GPIO.add_event_detect(pin_rain, GPIO.FALLING, callback = isr_rain, bouncetime = 50) 
 
 # Endlosschleife wie oben
 try:
-  while True:
-    # nix Sinnvolles tun
-    t += 1
-    print(f"time {t}")
-    if t == 60:
-      rainvolume = Counter_Rain * volPerKlick
+    while True:
+        # nix Sinnvolles tun
+        t += 1
+        # print(f"time {t}")
+        if t == 60:
+            if Counter_Rain > 0:
+                rainvolume = Counter_Rain * volPerKlick
 
-      ts = getInflxTimestamp()
-      write2influxapi(f'rain,type=gauge  volume={rainvolume} {ts}')
+                ts = getInflxTimestamp()
+                write2influxapi(f'rain,type=gauge  volume={rainvolume} {ts}')
 
-      ts = getOSMTimestamp()
-      osm_data = [
-      {"sensor": f"{rainID}","value": f"{rainvolume}","createdAt": f"{ts}"}
-      ]
-      postOSMvalues(osm_data)
-      postOpenhabValues(oh_rainID, rainvolume, ts)
+                ts = getOSMTimestamp()
+                osm_data = [
+                    {"sensor": f"{rainID}","value": f"{rainvolume}","createdAt": f"{ts}"}
+                ]
+                postOSMvalues(osm_data)
+                postOpenhabValues(oh_rainID, rainvolume, ts)
 
-      Counter_Rain = 0
-      t = 0
-    time.sleep(1)
+            rainvolume = 0
+            Counter_Rain = 0
+            t = 0
+        time.sleep(1)
 
 except KeyboardInterrupt:
   GPIO.cleanup()
--- get_wind.py
+++ get_wind.py
@@ -24,33 +24,36 @@ GPIO.setup(pin_wind, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 def isr_wind(channel):  
     global Counter_Wind
     Counter_Wind += 1
-#    print("Counter_Wind: %d" % Counter_Wind)
+    print("Counter_Wind: %d" % Counter_Wind)
 
 # Interrupts aktivieren
 GPIO.add_event_detect(pin_wind, GPIO.FALLING, callback = isr_wind, bouncetime = 50) 
 
 # Endlosschleife wie oben
 try:
-  while True:
-    # nix Sinnvolles tun
-    t += 1
-    print(f"time {t}")
-    if t == 60:
-      windspeed = (Counter_Wind / 2) / 60.0 * 2.4 # Counter / 60 Seconds * 2.4m/s
+    while True:
+        # nix Sinnvolles tun
+        t += 1
+        # print(f"time {t}")
+        if t == 60:
 
-      ts = getInflxTimestamp()
-      write2influxapi(f'wind,type=anemometer  volume={windspeed} {ts}')
+            if Counter_Wind > 0:
+                windspeed = (Counter_Wind / 2) / 60.0 * 2.4 # Counter / 60 Seconds * 2.4m/s
 
-      ts = getOSMTimestamp()
-      osm_data = [
-      {"sensor": f"{windspeedID}","value": f"{windspeed}","createdAt": f"{ts}"}
-      ]
-      postOSMvalues(osm_data)
-      postOpenhabValues(oh_windspeedID, windspeed, ts)
+                ts = getInflxTimestamp()
+                write2influxapi(f'wind,type=anemometer  volume={windspeed} {ts}')
 
-      Counter_Wind = 0
-      t = 0
-    time.sleep(1)
+                ts = getOSMTimestamp()
+                osm_data = [
+                    {"sensor": f"{windspeedID}","value": f"{windspeed}","createdAt": f"{ts}"}
+                ]
+                postOSMvalues(osm_data)
+                postOpenhabValues(oh_windspeedID, windspeed, ts)
+
+            windspeed = 0
+            Counter_Wind = 0
+            t = 0
+        time.sleep(1)
 
 except KeyboardInterrupt:
-  GPIO.cleanup()
+    GPIO.cleanup()
