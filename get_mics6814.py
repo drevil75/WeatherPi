01bfda7969dafc46b71a0a7912a0778a3b36d82c
Mirko Wust <mirko@Air-von-Mirko.fritz.box>
2023-01-06 10:49:10 +0100

    mics

---
+++ get_mics6814.py
@@ -0,0 +1,51 @@
+import RPi.GPIO as GPIO
+import spidev
+from time import sleep
+from apis.send2influxapi import *
+from apis.send2opensensemap import *
+from apis.send2openhab import *
+
+config = configparser.ConfigParser()
+config.read('./config.cfg')
+AnalogPin = int(config['sound']['sensorpin'])
+pin_voltage = float(config['sound']['pin_voltage'])
+
+# Initialisierung der Analogen Pins
+# A0,A1,A2,A3,A4,A5,A6,A7 = 0,1,2,3,4,5,6,7
+# pin_voltage = 3.3
+
+# SPI-Einstellungen
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 2000000
+
+def readadc(adcnum):
+# Auslesen der SPI-Werte
+ r = spi.xfer2([1,8+adcnum <<4,0])
+ adcout = ((r[1] &3) <<8)+r[2]
+ return adcout
+
+co_pin = 0
+nh2_pin = 1
+nh3_pin = 6
+
+def read_sound():
+   print('---------read_sound--------')
+   print(((readadc(AnalogPin) / 1024) * pin_voltage))
+   print(((readadc(AnalogPin) / 1024) * pin_voltage))
+   print(((readadc(AnalogPin) / 1024) * pin_voltage))
+
+   # ts = getInflxTimestamp()
+   # write2influxapi(f'dfrobot,type=sound  volume={val} {ts}')
+
+   # ts = getOSMTimestamp()
+   # osm_data = [
+   #    {"sensor": f"{soundID}","value": f"{val}","createdAt": f"{ts}"}
+   # ]
+   # postOSMvalues(osm_data)
+
+   # postOpenhabValues(oh_soundID, val, ts)
+
+
+if __name__ == "__main__":
+  read_sound()
