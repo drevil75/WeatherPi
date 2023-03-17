c15a57ba61a2fa06526cecc2918afa9b8c64fad6
sa_ucc-engineering <mirko@Air-von-Mirko.fritz.box>
2023-03-17 02:04:11 +0100

    main ctrl popen

--- main_ctrl.py
+++ main_ctrl.py
@@ -1,35 +1,22 @@
 import time
-from get_bme280 import *
-from get_dht22 import *
-from get_soundlevel import *
-from get_sps30 import *
-from get_tsl45315 import *
-from get_veml6070 import *
-
+import subprocess
 
 while True:
     try:
-        read_bme280()
+        subprocess.Popen(["python3", 'get_bme280.py'])
+        subprocess.Popen(["python3", 'get_hdc1080.py'])
+        subprocess.Popen(["python3", 'get_mics6814.py'])
+        subprocess.Popen(["python3", 'get_mq131.py'])
+        subprocess.Popen(["python3", 'get_rain.py'])
+        subprocess.Popen(["python3", 'get_scd30.py'])
+        subprocess.Popen(["python3", 'get_smt50.py'])
+        subprocess.Popen(["python3", 'get_soundlevel.py'])
+        subprocess.Popen(["python3", 'get_sps30.py'])
+        subprocess.Popen(["python3", 'get_tsl45315.py'])
+        subprocess.Popen(["python3", 'get_veml6070.py'])
+        subprocess.Popen(["python3", 'get_wind.py'])
+        time.sleep(60)
     except:
         pass
-    try:
-        read_dht22()
-    except:
-        pass
-    try:
-        read_sound()
-    except:
-        pass
-    try:
-        read_sps30()
-    except:
-        pass
-    try:
-        read_tsl45315()
-    except:
-        pass
-    try:
-        read_veml6070()
-    except:
-        pass
-    # time.sleep(60)
+
+    
