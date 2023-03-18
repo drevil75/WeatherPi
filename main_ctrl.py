c6574ab3219f9774652adf35cff0568e33090feb
sa_ucc-engineering <mirko@Air-von-Mirko.fritz.box>
2023-03-18 01:17:58 +0100

    0.0.8

--- main_ctrl.py
+++ main_ctrl.py
@@ -7,11 +7,10 @@ subprocess.Popen(['/usr/bin/python3', 'transferData.py'])
 while True:
     try:
         os.system('/usr/bin/python3 get_bme280.py')
-        os.system('/usr/bin/python3 get_cjmcu811.py')
+        # os.system('/usr/bin/python3 get_cjmcu811.py')
         os.system('/usr/bin/python3 get_hdc1080.py')
         os.system('/usr/bin/python3 get_mics6814.py')
         os.system('/usr/bin/python3 get_mq131.py')
-        
         os.system('/usr/bin/python3 get_scd30.py')
         os.system('/usr/bin/python3 get_smt50.py')
         os.system('/usr/bin/python3 get_soundlevel.py')
