eed5b162b858e5f4cfb48d89f3d2ec0234503027
sa_ucc-engineering <mirko@Air-von-Mirko.fritz.box>
2023-04-20 17:13:46 +0200

    fix wind and rain shebang remove

--- get_rain.py
+++ get_rain.py
@@ -37,34 +37,31 @@ def isr_rain(channel):
 GPIO.add_event_detect(pin_rain, GPIO.RISING, callback = isr_rain, bouncetime = 500) 
 
 # Endlosschleife wie oben
-try:
-    while True:
-        # nix Sinnvolles tun
-        t += 1
-        # print(f"time {t}")
-        if t == 60:
-            if Counter_Rain > 0:
-                rainvolume = Counter_Rain * volPerKlick
 
-                print(f'rain vol={rainvolume}')
+while True:
+    # nix Sinnvolles tun
+    t += 1
+    # print(f"time {t}")
+    if t == 60:
+        if Counter_Rain > 0:
+            rainvolume = Counter_Rain * volPerKlick
 
-                ts = getInflxTimestamp()
-                data = f'rain,type=gauge  volume={rainvolume} {ts}'
-                writeBuffer('influx-rain', data)
+            print(f'rain vol={rainvolume}')
 
-                ts = getOSMTimestamp()
-                osm_data = [
-                    {"sensor": f"{rainID}","value": f"{rainvolume}","createdAt": f"{ts}"}
-                ]
-                writeBuffer('osm-rain', json.dumps(osm_data))
+            ts = getInflxTimestamp()
+            data = f'rain,type=gauge  volume={rainvolume} {ts}'
+            writeBuffer('influx-rain', data)
 
-                # writeBuffer('openhab-rain', f'{oh_rainID},{rainvolume},{ts}')
-                postOpenhabValues(oh_rainID,rainvolume, ts)
+            ts = getOSMTimestamp()
+            osm_data = [
+                {"sensor": f"{rainID}","value": f"{rainvolume}","createdAt": f"{ts}"}
+            ]
+            writeBuffer('osm-rain', json.dumps(osm_data))
 
-            rainvolume = 0
-            Counter_Rain = 0
-            t = 0
-        time.sleep(1)
+            # writeBuffer('openhab-rain', f'{oh_rainID},{rainvolume},{ts}')
+            postOpenhabValues(oh_rainID,rainvolume, ts)
 
-except KeyboardInterrupt:
-  GPIO.cleanup()
+        rainvolume = 0
+        Counter_Rain = 0
+        t = 0
+    time.sleep(1)
--- get_wind.py
+++ get_wind.py
@@ -36,48 +36,45 @@ def isr_wind(channel):
 GPIO.add_event_detect(pin_wind, GPIO.FALLING, callback = isr_wind, bouncetime = 80) 
 
 # Endlosschleife wie oben
-try:
-    while True:
-        # nix Sinnvolles tun
-        t += 1
-        # print(f"time {t}")
-        if t == 60:
+
+while True:
+    # nix Sinnvolles tun
+    t += 1
+    # print(f"time {t}")
+    if t == 60:
+
+        if Counter_Wind > 0:
+            windspeed = ((Counter_Wind) / 60.0 * 2.4) * 1.18 # Counter / 60 Seconds * 2.4m/s (1.18 is anemometer factor - loss of wind force)
 
             if Counter_Wind > 0:
-                windspeed = ((Counter_Wind) / 60.0 * 2.4) * 1.18 # Counter / 60 Seconds * 2.4m/s (1.18 is anemometer factor - loss of wind force)
+                bouncetime = 100
+            if Counter_Wind > 100:
+                bouncetime = 50
+            if Counter_Wind > 500:
+                bouncetime = 30
+            if Counter_Wind > 1000:
+                bouncetime = 20
+            if Counter_Wind > 1500:
+                bouncetime = 10
+            GPIO.remove_event_detect(pin_wind)
+            GPIO.add_event_detect(pin_wind, GPIO.RISING, callback = isr_wind, bouncetime = bouncetime)
 
-                if Counter_Wind > 0:
-                    bouncetime = 100
-                if Counter_Wind > 100:
-                    bouncetime = 50
-                if Counter_Wind > 500:
-                    bouncetime = 30
-                if Counter_Wind > 1000:
-                    bouncetime = 20
-                if Counter_Wind > 1500:
-                    bouncetime = 10
-                GPIO.remove_event_detect(pin_wind)
-                GPIO.add_event_detect(pin_wind, GPIO.RISING, callback = isr_wind, bouncetime = bouncetime)
+            print(f'wind speed={windspeed}')
+            
+            ts = getInflxTimestamp()
+            data = f'wind,type=anemometer  speed={windspeed} {ts}'
+            writeBuffer('influx-wind', data)
 
-                print(f'wind speed={windspeed}')
-                
-                ts = getInflxTimestamp()
-                data = f'wind,type=anemometer  speed={windspeed} {ts}'
-                writeBuffer('influx-wind', data)
+            ts = getOSMTimestamp()
+            osm_data = [
+                {"sensor": f"{windspeedID}","value": f"{windspeed}","createdAt": f"{ts}"}
+            ]
+            writeBuffer('osm-wind', json.dumps(osm_data))
 
-                ts = getOSMTimestamp()
-                osm_data = [
-                    {"sensor": f"{windspeedID}","value": f"{windspeed}","createdAt": f"{ts}"}
-                ]
-                writeBuffer('osm-wind', json.dumps(osm_data))
+            # writeBuffer('openhab-wind', f'{oh_windspeedID},{windspeed},{ts}')
+            postOpenhabValues(oh_windspeedID,windspeed, ts)
 
-                # writeBuffer('openhab-wind', f'{oh_windspeedID},{windspeed},{ts}')
-                postOpenhabValues(oh_windspeedID,windspeed, ts)
-
-            windspeed = 0
-            Counter_Wind = 0
-            t = 0
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+        windspeed = 0
+        Counter_Wind = 0
+        t = 0
+    time.sleep(1)
